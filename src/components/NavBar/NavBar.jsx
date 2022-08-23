3c2e58851c4ffbbf01b7abc6530139bb107a1d25
Oby <obyli214@gmail.com>
2022-08-23 01:56:03 -0700

    added Home page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,25 +4,28 @@ import * as userService from "../../utilities/users-service";
 export default function NavBar({ user, setUser }) {
   return (
     <nav>
+      <div className="Avatar-info-wrapper">
+        <div className="Avatar"></div>
+        <div className="white name">Oby Li</div>
+        <div className="white role">Full Stack Developer</div>
+      </div>
+      <hr />
+      <div className="Routes-Wrapper">
       <div>
-        <div></div>
-        <div>Oby Li</div>
-        <div>Full Stack Developer</div>
+        <Link className="nav-tags HomeHref" to="/">Home</Link>
       </div>
       <div>
-        <Link to="/">Home</Link>
+        <Link className="nav-tags AboutHref" to="/about">About</Link>
       </div>
       <div>
-        <Link to="/about">About</Link>
+        <Link className="nav-tags PortfolioHref" to="/portfolio">Portfolio</Link>
       </div>
       <div>
-        <Link to="/portfolio">Portfolio</Link>
+        <Link className="nav-tags ExperiencesHref" to="/experiences">Experiences</Link>
       </div>
-      <div>
-        <Link to="/experiences">Experiences</Link>
-      </div>
-      <Link to="/contact">Contact</Link>
+      <Link className="nav-tags ContactHref" to="/contact">Contact</Link>
       <div></div>
+      </div>
     </nav>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
     return (
     <div className="Routes">
-        <h1>Home Page</h1>
+        <div className="Home-Info-Wrapper">
+            <div className="Home-Info">
+            <h1 className="white HomeName">Hello, I'm Oby</h1>
+            <p className="white paragraph">I am a full-stack Software Developer who enjoys and have fun creating custom designs, functionality, and animation for web-apps</p>
+            <Link className="ClickAbout" to="/about">Click here to know more about me!</Link>
+            </div>
+        </div>
     </div>
     )
 }
