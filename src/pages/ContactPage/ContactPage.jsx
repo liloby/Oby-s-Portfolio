57e704d8cb826e78d095c5ecd00a1ecdc125f9a6
Oby <obyli214@gmail.com>
2022-08-28 23:53:32 -0700

    setup message form

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,9 +1,25 @@
 export default function ContactPage() {
     return (
         <div className="Routes">
-            <h1 className="white">
-                Contact Page
-            </h1>
+            <div className="message">
+                <h1 className="white">Contact Me</h1>
+            <form onSubmit="">
+                <label className="white">Name:</label>
+                <input type="text" required/>
+                <label className="white">Email:</label>
+                <input type="email" required/>
+                <label className="white">Phone (Optional):</label>
+                <input type="tel" name="" id="" />
+                <label className="white">Message:</label>
+                <textarea name="" id="" cols="30" rows="10" required></textarea>
+                <button type="submit">Send</button>
+            </form>
+            <div>
+                <div>GitHub</div>
+                <div>LinkedIn</div>
+                <div>Email</div>
+            </div>
+            </div>
         </div>
     )
 }
